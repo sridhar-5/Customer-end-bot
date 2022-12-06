80f2024ff14b5ea8939aba7924f845ba75b8c386
Venkata Sridhar Sai Kalavalapalli <kv.sridharsai@gmail.com>
2022-12-07 00:18:50 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,58 @@
+const dotenv = require("dotenv");
+const express = require("express");
+const session = require("express-session");
+const app = express();
+const bodyParser = require("body-parser");
+const Twilio = require("twilio");
+const connectDatabase = require("./config/DbConnection");
+const logModel = require("./Models/logs");
+const handleWhatsAppMessages = require("./BotHandlingMethods/handleWhatsAppMessages");
+if (typeof localStorage === "undefined" || localStorage === null) {
+  var LocalStorage = require("node-localstorage").LocalStorage;
+  localStorage = new LocalStorage("./localStorage");
+}
+
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+
+var accountSid = process.env.TWILIO_ACCOUNT_SID;
+var authToken = process.env.TWILIO_AUTH_TOKEN;
+const client = require("twilio")(accountSid, authToken);
+
+app.use(bodyParser.json());
+app.use(session({ secret: process.env.COOKIE_SECRET }));
+
+dotenv.config();
+
+// function call to connect to the mongo database
+connectDatabase();
+
+app.get("/", (request, response) => {
+  response.status(200).send("Hello..! This is just to test.");
+});
+
+app.post(
+  "/push_notifications_from_message_queue",
+  async (request, response) => {
+    // const messageRequestBody = request.body;
+
+    // let twiml = new Twilio.twiml.MessagingResponse();
+    // // response to the user in thw whats app bot
+    // result.forEach((message) => {
+    //   twiml.message(message);
+    // });
+
+    // response.end(twiml.toString());
+
+    response.send("Hello..! This is just to test.");
+  }
+);
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`server started on ${PORT}`);
+});
